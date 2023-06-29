53fc019ef177c560e451b984b40688e50184583b
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-06-28 22:08:49 -0300

    preenchi mais uns dados para serem tratados no construtor de produtos

--- lib/models/produto.dart
+++ lib/models/produto.dart
@@ -1,19 +1,23 @@
 class Produto {
+  final int id;
   final String foto;
-  final String nome;
+  final String titulo;
   final String descricao;
   final double valor;
+  final int categoriaId;
+  final String categoriaNome;
 
-  Produto(
-      {required this.foto,
-      required this.nome,
-      required this.descricao,
-      required this.valor});
+  Produto({
+    required this.foto,
+    required this.titulo,
+    required this.descricao,
+    required this.valor,
+  });
 
   factory Produto.fromJson(Map<String, dynamic> json) {
     return Produto(
         foto: json['foto'],
-        nome: json['nome'],
+        titulo: json['titulo'],
         descricao: json['descricao'],
         valor: json['valor']);
   }
